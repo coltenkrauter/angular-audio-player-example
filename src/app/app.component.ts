e91a14a4161f7a1589ba7180aacffa4aadbac76b
coltenkrauter <coltenkrauter@gmail.com>
2019-11-23 15:52:56 -0800

    Add ngx-plyr audio/video player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { PlyrComponent } from 'ngx-plyr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'audio-player';
+   // get the component instance to have access to plyr instance
+  @ViewChild(PlyrComponent, {static: false}) Plyr: PlyrComponent;
+
+  // or get it from plyrInit event
+  player: Plyr;
+
+  videoSources: Plyr.Source[] = [
+    {
+      src: 'bTqVqk7FSmY',
+      provider: 'youtube',
+    },
+  ];
+
+  played(event: Plyr.PlyrEvent) {
+    console.log('played', event);
+  }
+
+  play(): void {
+    this.player.play(); // or this.plyr.player.play()
+  }
 }
